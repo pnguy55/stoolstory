333940835384a43e7c2e4d76556716260f789f55
Phi Nguyen <pnguy55@gmail.com>
2020-12-11 21:06:54 -0600

    add new favicon

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ require('../models/Email');
 require('../services/passport');
 
 
-// mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
 //uncomment HTTPS for production
